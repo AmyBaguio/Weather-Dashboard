143f3517f057b101bd1c9c8a3af3185f4f634b86
amalyn <amalynlaquinta@yahoo.com>
2020-10-05 15:42:40 +0800

    90% completed. still need to add UV index

--- weather.js
+++ weather.js
@@ -1,112 +1,185 @@
 
 
-// var APIKey = "88afaf5d902bd0951e5afcfd34451691";
+// To display current city search result
 
-
-function createForeCastCard(date, icon, temp, humidity) {
-
-    let card = $("<div>")
-    card.attr("class", "card")
-
-    let cardBody = $("<div>")
-    cardBody.attr("class", "card-body");
-
-    let cardTitle = $("<h4>")
-    cardTitle.attr("class", "card-title");
-    cardTitle.text(date);
-    cardBody.append(cardTitle);
-
-    let img = $("<img>");
-    img.attr("src", icon);
-    cardBody.append(img);
-
-    // crete the p tags
-    let cardText = $("<p>")
-    cardText.attr("class", "card-text");
-    cardText.text(date);
-    cardBody.append(cardTitle);
-
-
-    card.append(cardBody);
-
-    return card;
-
-}
-
-
-$("#find-city").on("click", function (event) {
-    event.preventDefault();
-
-    // Here we grab the text from the input box
-    const city = $("#city-input").val().trim();
-
-
-    // current weather forecast URL //
-    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=88afaf5d902bd0951e5afcfd34451691";
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-
-        $(".city").html("<h1>" + response.name + "</h1>");
-        $(".icon").text("icon" + response.weather.icon);
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-        // Log the data in the console as well
-
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
-    });
-
-});
 
 // Search History List View
+$(document).ready(function () {
+
+    function createIconUrl(iconcode) {
+        return "https://openweathermap.org/img/w/" + iconcode + ".png";
+
+    }
+    function searchWeather(city) {
 
 
+        // current weather forecast URL //
+        const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=88afaf5d902bd0951e5afcfd34451691";
 
-// Displaying 5-Day Forecast
 
-$("#find-city").on("click", function (event) {
-    event.preventDefault();
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
 
-    // Here we grab the text from the input box
-    const city = $("#city-input").val().trim();
+            $(".city").html("<h1>" + response.name + "</h1>");
+            $(".icon").attr("src", createIconUrl(response.weather[0].icon));
+            $(".wind").text("Wind Speed: " + response.wind.speed);
+            $(".humidity").text("Humidity: " + response.main.humidity);
 
-    // 5 Day ForeCast URL
-    const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=88afaf5d902bd0951e5afcfd34451691";
+            // Convert the temp to fahrenheit
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+            // add temp content to html
+            $(".temp").text("Temperature (K) " + response.main.temp);
+            $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-        const results = response.list;
+            // Log the data in the console as well
 
-        let dailyForecast = []
-        for (let index = 0; index < results.length; index += 8) {
-            const element = results[index];
-            dailyForecast.push(element)
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + tempF);
+        });
+
+    };
+
+    const APIKey = "88afaf5d902bd0951e5afcfd34451691";
+
+    renderHistoryList();
+
+    $("#find-city").on("click", function (event) {
+        event.preventDefault();
+
+        // get the user input
+        const citySearch = $("#city-input").val();
+
+        // save user input to local storage
+
+        // get the existing value in LS
+        const existed = JSON.parse(localStorage.getItem('historyItems')) || [];
+
+        // add to the list
+        existed.push(citySearch);
+
+        // save the updated list in LS
+        localStorage.setItem('historyItems', JSON.stringify(existed));
+
+        // call the api
+        //clear input box
+        searchWeather(citySearch);
+
+        searchForecasts(citySearch);
+        $("#city-input").val("");
+        renderHistoryList()
+
+    });
+
+    function renderHistoryList() {
+
+        // clear the content of history items
+        $(".historyItems").empty();
+
+        // get all the items in LS,
+        const historyList = JSON.parse(localStorage.getItem('historyItems')) || [];
+
+        // for each item, we generate a li element
+        for (let index = 0; index < historyList.length; index++) {
+            const city = historyList[index];
+            const li = $("<li>").addClass("list-group-item list-group-item-action").text(city);
+
+            // append li to '.historyItems'
+            $(".historyItems").append(li);
         }
 
-        console.log(dailyForecast)
 
-        for (let index = 0; index < dailyForecast.length; index++) {
-            const element = dailyForecast[index];
-            const card = createForeCastCard(moment(element.dt_txt).format('YYYY'), "", 15, 60)
-            $('.forecast-view').append(card);
-        }
-    })
-});
+    }
+
+
+    $(".historyItems").on("click", "li", function (event) {
+        event.preventDefault();
+
+        // story 
+
+        // when we click on a city
+        const city = $(event.target).text();
+
+        // we call the api based on that city
+        searchWeather(city);
+
+        searchForecasts(city);
+
+    });
+
+
+
+    // Creating Forecast Card
+    function createForeCastCard(date, icon, temp, humidity) {
+    
+        let card = $("<div>")
+        card.attr("class", "card col")
+    
+        let cardBody = $("<div>")
+        cardBody.attr("class", "card-body");
+    
+        let cardTitle = $("<h4>")
+        cardTitle.attr("class", "card-title");
+        cardTitle.text(date);
+        cardBody.append(cardTitle);
+    
+        let img = $("<img>");
+        img.attr("src", icon);
+        cardBody.append(img);
+    
+        // crete the p tags
+        let cardTemp = $("<p>")
+        cardTemp.attr("class", "card-text");
+        cardTemp.text('Temp: ' + temp + 'K');
+        cardBody.append(cardTemp);
+    
+        
+        let cardHumidity = $("<p>")
+        cardHumidity.attr("class", "card-text");
+        cardHumidity.text(humidity);
+        cardBody.append(cardHumidity);
+    
+    
+        card.append(cardBody);
+    
+        return card;
+    
+    }
+    // Displaying 5-Day Forecast
+    
+    function searchForecasts(city) {
+
+        $(".forecast-view").empty();
+    
+        // 5 Day ForeCast URL
+        const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=88afaf5d902bd0951e5afcfd34451691";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+    
+            const results = response.list;
+    
+            let dailyForecast = []
+            for (let index = 0; index < results.length; index += 8) {
+                const element = results[index];
+                dailyForecast.push(element)
+            }
+    
+            console.log(dailyForecast)
+    
+            for (let index = 0; index < dailyForecast.length; index++) {
+                const element = dailyForecast[index];
+                const card = createForeCastCard(moment(element.dt_txt).format('YYYY-MM-DD'), createIconUrl(element.weather[0].icon), element.main.temp, element.main.humidity)
+                $('.forecast-view').append(card);
+               
+            }
+        })
+    };
+})
